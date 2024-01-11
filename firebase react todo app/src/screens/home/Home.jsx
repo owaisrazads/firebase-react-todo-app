ab4789224ce57f18dbb060ba071a8cc1ea1d9231
unknown <oraza7969@gmail.com>
2024-01-11 10:58:07 -0800

    react firebase todo app

--- firebase react todo app/src/screens/home/Home.jsx
+++ firebase react todo app/src/screens/home/Home.jsx
@@ -1,5 +1,5 @@
-import { signOut, onAuthStateChanged  } from "firebase/auth";
-import {auth} from "../../config/firebase-config/FirebaseConfig"
+import { signOut, onAuthStateChanged } from "firebase/auth";
+import { auth } from "../../config/firebase-config/FirebaseConfig"
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 
@@ -7,22 +7,64 @@ import { useNavigate } from 'react-router-dom';
 
 
 const Home = () => {
-  const [text, setText] = useState('')
-  const [todo, setTodo] = useState('')
-  const [update, setUpdate] = useState('')
+
+  const [text, setText] = useState("")
+  const [todo, setTodo] = useState([])
   const navigate = useNavigate('')
 
+
+
+  function addTodo(event) {
+    event.preventDefault();
+    console.log(text);
+    if (text !== "") {
+      todo.push(text)
+      setTodo([...todo]);
+      console.log(todo);
+      setText ("")
+    }else{
+      alert('Please enter something')
+    }
+  }
+  
+ //function delete Todo
+
+
+
+function deleteTodo(index) {
+  const deleteTodo = [...todo];
+  deleteTodo.splice(index, 1)
+  setTodo(deleteTodo)
+
+  
+}
+  
+ //function Edit Todo
+
+function updateTodo(index) {
+  const updatedText = prompt('Update Todo', todo[index]);
+  if (updatedText === '') {
+    alert('Please enter something')
+    
+  }else{
+    const updatedTodos = [...todo];
+    updatedTodos[index] = updatedText;
+    setTodo(updatedTodos)
+  }
+}
 
   //onauth state change
   onAuthStateChanged(auth, (user) => {
     if (user) {
-   
+
       const uid = user.uid;
       console.log(uid);
     } else {
-     navigate('login')
+      navigate('login')
     }
   });
+
+  //logout btn
 
   function logoutBtn() {
     console.log('btn called');
@@ -38,33 +80,42 @@ const Home = () => {
   }
 
   return (
-   <>
-    <div className="navbar bg-blue-800">
-      <div className="flex-1">
-        <a className="text-xl text-white font-semibold">React Todo App</a>
+    <>
+      <div className="navbar bg-blue-800">
+        <div className="flex-1">
+          <a className="text-xl text-white font-semibold">React Todo App</a>
+        </div>
+        <div className="flex-none">
+          <button className="btn btn-info mr-5" onClick={logoutBtn}>Logout</button>
+        </div>
       </div>
-      <div className="flex-none">
-        <button className="btn btn-info" onClick={logoutBtn}>Logout</button>
-      </div>
-    </div>
 
-    <div className="flex justify-center mt-5">
-      <form>
-      <label class="form-control w-[340px] max-w-xs mb-5">
-                    <input type="email" id="email" placeholder="Enter Todo" class="input border-solid border-2 p-3 border-gray-300 w-full max-w-xs"/>
-                </label>
-                <button className="btn btn-info" >Add Todo</button>
-                
-
-                
+      <div className="flex justify-center mt-5">
+      <form onSubmit={addTodo}>
+      <div className="flex">
+      <input type="text" id="email" placeholder="Enter todo" class="input border-solid border-2 p-3 border-gray-300 w-[20rem] max-w-xs" onChange={(e)=>setText(e.target.value)} value={text} />
+      <button type="submit" class="btn btn-info mb-5 ml-3 w-[65px]">Add todo</button>
+     </div>
       </form>
-    </div>
-   </>
+      <hr />
+      </div>
 
-    
+       <ul >
+        {todo.map((item, index)=>{
+          return <li key={index}>{item}
+          <button className='dlt-btn rounded-lg ml-[20rem] py-1  px-3 bg-red-500 text-white' onClick={()=>deleteTodo(index)}><i class="fa-solid fa-trash-arrow-up"></i> </button>
+          <button className='edit-btn rounded-lg py-1  px-3 bg-blue-500 text-white ml-2' onClick={()=>updateTodo(index)}><i class="fa-solid fa-pencil"></i> </button><br></br><br /><hr/>
+          </li>
+        })}
+      </ul> 
+    </>
+
+
   )
 
-  
-  }
+
+}
 
 export default Home
+
+// onChange={(e)=>setText(e.target.value)} value={text}
